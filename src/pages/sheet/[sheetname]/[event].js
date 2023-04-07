f53d666eb1f38ca283b3094ddf2e082ec145b875
sejinsjn <sedad_@hotmail.de>
2023-04-08 00:22:46 +0200

    Added table for every event with all info

--- src/pages/sheet/[sheetname]/[event].js
+++ src/pages/sheet/[sheetname]/[event].js
@@ -1,8 +1,8 @@
-import Nav from "../nav";
-import styles from "../components/Sheetname.module.css"
+import Nav from "../../nav";
 import { google } from 'googleapis';
 import Image from 'next/image'
 import Link from 'next/link'
+import { Router } from 'next/router';
 
 export async function getServerSideProps({ query }) {
 
@@ -20,7 +20,9 @@ export async function getServerSideProps({ query }) {
 
     const googlesheets = google.sheets({ version: 'v4', auth: jwt });
 
+    const { event } = query;
     const { sheetname } = query;
+
     var spreadsheetId = "";
 
     switch (sheetname) {
@@ -40,18 +42,13 @@ export async function getServerSideProps({ query }) {
             spreadsheetId = process.env.SHEET_FORTRADE;
     }
 
-    const spreadsheet = await googlesheets.spreadsheets.get({
-        spreadsheetId
+    const range = `${event}!A4:S`;
+    const sheet = await googlesheets.spreadsheets.values.get({
+        spreadsheetId,
+        range
     });
 
-    const sheets = spreadsheet.data.sheets;
-
-    const sheetnames = spreadsheet.data.sheets.map((sheet) => {
-        return sheet.properties.title
-    });
-
-
-
+    const sheetJson = sheet.data.values;
 
     /*
     for (const sheet of sheets) {
@@ -61,6 +58,7 @@ export async function getServerSideProps({ query }) {
         const item = [];
         const range = `${ title }!A4:P4`;
     }
+
     const events = [];
     const pokedexNr = [];
     const shiny = [];
@@ -96,35 +94,63 @@ export async function getServerSideProps({ query }) {
 
     return {
         props: {
-            sheetname,
-            sheetnames
+            sheetJson
         }
     }
 }
 
-function Card(sheet, sheetnames) {
-    var id = 4;
-    const listEvents = sheetnames.map((sheetname) =>
-        <div className="event-card" key={ id++ }>
-            
-            <Link className="card-body" href={{
-                pathname: `/sheet/[sheet]/[sheetname]`,
-                query: { sheetname: sheetname }
-            }}
-                as={`/sheet/${sheet}/${sheetname}`}>
-                <h6 className="card-title">{sheetname}</h6>
-            </Link>
-        </div>
-    );
+function JsonDataDisplay(sheetJson) {
+    console.log(sheetJson[0][0]);
+    const DisplayData = sheetJson.map(
+        (row) => 
+            <tr>
+                <td>{row[1]}</td>
+                <td>{row[2]}</td>
+                <td>{row[5]}</td>
+                <td>{row[7]}</td>
+                <td>{row[8]}</td>
+                <td>{row[9]}</td>
+                <td>{row[10]}</td>
+                <td>{row[11]}</td>
+                <td>{row[12]}</td>
+                <td>{row[13]}</td>
+                <td>{row[14]}</td>
+                <td>{row[15]}</td>
+                <td>{row[16]}</td>
+                <td>{row[18]}</td>
+            </tr >
+        )
 
-    return <div className="event-container">
-        {listEvents}
-    </div>
+    return <tbody>
+        {DisplayData}
+    </tbody>
 }
 
-export default function Post({ sheetname, sheetnames }) {
+export default function Post({ sheetJson }) {
     return <div>
         <Nav />
-        {Card(sheetname, sheetnames) }
+        <div>
+            <table className="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Marking</th>
+                        <th scope="col">Lang</th>
+                        <th scope="col">Ball</th>
+                        <th scope="col">Ability</th>
+                        <th scope="col">Nature</th>
+                        <th scope="col">Lvl</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">IVs</th>
+                        <th scope="col">OT/ID</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Proof</th>
+                        <th scope="col">Trade History</th>
+                        <th scope="col">Disclosure</th>
+                        <th scope="col">Rule3 CopyPasta</th>
+                    </tr>
+                </thead>
+                    {JsonDataDisplay(sheetJson)}
+            </table>
+        </div>
     </div>
 }
