a9f5c41bfe9e468df59a7a5456545ad007a0f6ae
sejinsjn <sedad_@hotmail.de>
2023-04-15 03:13:48 +0200

    some css changes for nav

--- src/pages/components/homeNav.js
+++ src/pages/components/homeNav.js
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 
 export default function HomeNav() {
-    return <nav className="nav">
-        <ul>
-            <li className="nav-item">
-                <Link className="" aria-current="page" href="#home">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="" href="#aboutme">About me</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="" href="#mywishlist">My Wishlist</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="" href="#mypokemon">My Pokemon</Link>
-            </li>
-        </ul>
-    </nav>
+    return <header>
+            <nav className="nav">
+                <ul>
+                    <li className="nav-item">
+                        <Link className="" aria-current="page" href="#home">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="" href="#aboutme">About me</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="" href="#mywishlist">My Wishlist</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="" href="#mypokemon">My Pokemon</Link>
+                    </li>
+                </ul>
+            </nav>
+    </header>
 }
