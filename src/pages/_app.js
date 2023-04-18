c2e0b22481609a6c3e66aa7005c505ff51320f48
sejinsjn <sedad_@hotmail.de>
2023-04-18 18:27:17 +0200

    vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import '@/styles/global.css'
+import { Analytics } from '@vercel/analytics/react';
 
+export function reportWebVitals(metric) {
+    console.log(metric)
+}
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics/>
+        </>
+    );
 }
