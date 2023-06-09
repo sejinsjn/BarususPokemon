0d84e6c530269ada46d3546e6b60c734243c0d81
sejinsjn <sedad_@hotmail.de>
2023-06-10 00:25:45 +0200

    fix?

--- components/Sprite.js
+++ components/Sprite.js
@@ -2,30 +2,26 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Sprite({ pokedexNr, isShiny, height, width }) {
-    if (pokedexNr === undefined || pokedexNr === "") return <p></p>;
-
-    var url = "";
-    var fallback = "";
-
     const swshNormalURL = `https://www.serebii.net/swordshield/pokemon/${pokedexNr}.png`;
     const svNormalURL = `https://www.serebii.net/scarletviolet/pokemon/new/${pokedexNr}.png`;
     const swshShinyURL = `https://www.serebii.net/Shiny/SWSH/${pokedexNr}.png`;
     const svShinyURL = `https://www.serebii.net/Shiny/SV/new/${pokedexNr}.png`;
 
-    if (isShiny == "TRUE") {
-        url = svShinyURL;
-        fallback = swshShinyURL;
-    } else {
-        url = svNormalURL;
-        fallback = swshNormalURL;
-    }
+    const [imageSrc, setImageSrc] = useState(svNormalURL);
+    const fallback = isShiny === "TRUE" ? swshShinyURL : swshNormalURL;
 
-
-    const [imageSrc, setImageSrc] = useState(url);
+    useEffect(() => {
+        const url = isShiny === "TRUE" ? svShinyURL : svNormalURL;
+        setImageSrc(url);
+    }, [pokedexNr, isShiny, svNormalURL, svShinyURL]);
 
     const handleImageError = () => {
         setImageSrc(fallback);
     };
+
+    if (pokedexNr === undefined || pokedexNr === "") {
+        return <p></p>;
+    }
 
     return (
         <Image
